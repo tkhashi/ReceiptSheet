96132b145094c3217881476c352c5eeee82e3b94
KazuhiroTakahashi <k_takahashi@just-ltd.co.jp>
2021-10-23 14:36:32 +0900

    first commit

---
+++ CloudVisionOCR/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Google.Cloud.Vision.V1;    
+
+namespace CloudVisionOCR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Image image2 = Image.FetchFromUri("https://www.nishinippon.co.jp/uploads/image/214089/large_PN2019082101001926.-.-.CI0003.jpg");
+            ImageAnnotatorClient client = ImageAnnotatorClient.Create();
+
+            IReadOnlyList<EntityAnnotation> textAnnotations = client.DetectText(image2);
+            foreach (EntityAnnotation text in textAnnotations)
+            {
+                Console.WriteLine($"Description: {text.Description}");
+            }
+        }
+    }
+}
